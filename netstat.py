77d02addf647d834ec0799bbbe3679dd06326d09
hblf <piero8606@126.com>
2016-03-09 21:05:26 +0800

    Update netstat.py

--- netstat.py
+++ netstat.py
@@ -1,14 +1,18 @@
+# -*-coding:utf-8 -*-
 import os
 import time
 
-net=os.popen('netstat -an').read()
+net=os.popen('netstat -an').read()  #获取网络连接信息
 
-time=time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+a=time.time()+46800            #计算时间并调整时差
 
-with open('sslog.txt','a') as f:
+ThisTime=time.localtime(a)
 
-	f.write(time+"\n\n\n")
+time=time.strftime('%Y-%m-%d %H:%M:%S',ThisTime)   #格式化时间time.localtime(time.time())
 
+with open('sslog.txt','a') as f:   #把当前时间写入日志文件
 
-with open('sslog.txt','a') as f:
+	f.write("\n\n"+time+"\n\n")
+
+with open('sslog.txt','a') as f:   #写入网络连接信息关闭日志文件
 	f.write(net)
